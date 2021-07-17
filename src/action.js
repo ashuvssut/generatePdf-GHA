6e540effed0ad3ec978f9c18049660f850e2b9c5
ashuvssut <ashu.khanduala@gmail.com>
2021-07-17 17:51:56 +0530

    upload to repo action

--- src/action.js
+++ src/action.js
@@ -1,5 +1,29 @@
-const run = async() => {
-  console.log('hayo')
-}
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-run();
+const getCurrentCommit = async (octokit, owner, repo, branch = "main") => {
+	const { data: refData } = await octokit.rest.git.getRef({
+		owner,
+		repo,
+		ref: `heads/${branch}`,
+	});
+	const commitSha = refData.object.sha;
+	console.log(commitSha)
+};
+
+const uploadToRepo = async (octokit, filePath, owner, repo, branch = `main`) => {
+	const currentCommit = await getCurrentCommit(octokit, owner, repo, branch);
+};
+
+const main = async () => {
+	const GITHUB_TOKEN = "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";
+	const octokit = github.getOctokit(GITHUB_TOKEN);
+
+	const OWNER = "ashuvssut";
+	const REPO = "ashuvssut-resume";
+	const BRANCH = "download";
+
+	await uploadToRepo(octokit, "./", OWNER, REPO, BRANCH);
+};
+
+main();
