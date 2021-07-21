1ff2d77f52197f48d80db96315fe8a57140aa6fe
ashuvssut <ashu.khanduala@gmail.com>
2021-07-21 15:49:29 +0530

    using puppeteer core

--- src/action.js
+++ src/action.js
@@ -1,86 +1,98 @@
 const github = require("@actions/github");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 
 // for testing only
 // require("dotenv").config();
 
 const getFileSha = async (octokit, pdfPath, owner, repo, branch = "main") => {
-	const pdfContent = await octokit.rest.repos.getContent({
-		owner,
-		repo,
-		path: pdfPath,
-		ref: `heads/${branch}`,
-	});
-	return pdfContent.data.sha;
+  const pdfContent = await octokit.rest.repos.getContent({
+    owner,
+    repo,
+    path: pdfPath,
+    ref: `heads/${branch}`,
+  });
+  return pdfContent.data.sha;
 };
 
 const getPdfBase64 = async () => {
-	const URL = "https://ashuvssut.github.io/ashuvssut-resume/";
-	const domSelector = "#resume-wrap";
-	const browser = await puppeteer.launch({ args: ["--no-sandbox", "--disable-setuid-sandbox"] });
-	const page = await browser.newPage();
-	await page.goto(URL, { waitUntil: "networkidle2" });
-	const desiredHtml = await page.$eval(domSelector, element => {
-		return element.innerHTML;
-	});
+  const URL = "https://ashuvssut.github.io/ashuvssut-resume/";
+  const domSelector = "#resume-wrap";
+  const browserFetcher = puppeteer.createBrowserFetcher();
+  let revisionInfo = await browserFetcher.download("884014");
 
-	await page.setContent(desiredHtml, { waitUntil: "networkidle2" });
-	await page.evaluateHandle("document.fonts.ready");
+  browser = await puppeteer.launch({
+    executablePath: revisionInfo.executablePath,
+    args: ["--no-sandbox", "--disabled-setupid-sandbox"],
+  });
+  const page = await browser.newPage();
+  await page.goto(URL, { waitUntil: "networkidle2" });
+  const desiredHtml = await page.$eval(domSelector, (element) => {
+    return element.innerHTML;
+  });
 
-	// create a pdf buffer with my preffered settings
-	const pdfBuffer = await page.pdf({
-		format: "A4",
-		landscape: false,
-		pageRanges: "1",
-		printBackground: true,
-		margin: "none",
-	});
-	await browser.close()
-	return pdfBuffer.toString("base64");
+  await page.setContent(desiredHtml, { waitUntil: "networkidle2" });
+  await page.evaluateHandle("document.fonts.ready");
+
+  // create a pdf buffer with my preffered settings
+  const pdfBuffer = await page.pdf({
+    format: "A4",
+    landscape: false,
+    pageRanges: "1",
+    printBackground: true,
+    margin: "none",
+  });
+  await browser.close();
+  return pdfBuffer.toString("base64");
 };
 
 const getDateTime = () => {
-	let currentdate = new Date();
-	return (datetime =
-		"Last Sync: " +
-		currentdate.getDate() +
-		"/" +
-		(currentdate.getMonth() + 1) +
-		"/" +
-		currentdate.getFullYear() +
-		" @ " +
-		currentdate.getHours() +
-		":" +
-		currentdate.getMinutes());
+  let currentdate = new Date();
+  return (datetime =
+    "Last Sync: " +
+    currentdate.getDate() +
+    "/" +
+    (currentdate.getMonth() + 1) +
+    "/" +
+    currentdate.getFullYear() +
+    " @ " +
+    currentdate.getHours() +
+    ":" +
+    currentdate.getMinutes());
 };
 
-const uploadToRepo = async (octokit, pdfPath, pdfBase64, owner, repo, branch = `main`) => {
-	const fileSha = await getFileSha(octokit, pdfPath, owner, repo, branch);
-	const datetime = getDateTime();
-	const result = await octokit.rest.repos.createOrUpdateFileContents({
-		owner,
-		repo,
-		branch,
-		message: `updating Resume pdf, ${datetime}`,
-		path: pdfPath,
-		content: pdfBase64,
-		sha: fileSha,
-	});
-	console.log(`Created commit at ${result.data.commit.html_url}`);
+const uploadToRepo = async (
+  octokit,
+  pdfPath,
+  pdfBase64,
+  owner,
+  repo,
+  branch = `main`
+) => {
+  const fileSha = await getFileSha(octokit, pdfPath, owner, repo, branch);
+  const datetime = getDateTime();
+  const result = await octokit.rest.repos.createOrUpdateFileContents({
+    owner,
+    repo,
+    branch,
+    message: `updating Resume pdf, ${datetime}`,
+    path: pdfPath,
+    content: pdfBase64,
+    sha: fileSha,
+  });
+  console.log(`Created commit at ${result.data.commit.html_url}`);
 };
 
 const main = async () => {
+  let GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
-	let GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  const octokit = github.getOctokit(GITHUB_TOKEN);
 
-	const octokit = github.getOctokit(GITHUB_TOKEN);
-
-	const OWNER = "ashuvssut";
-	const REPO = "ashuvssut-resume";
-	const BRANCH = "download";
-	const PDF_PATH = "Resume _ Ashutosh Khanduala.pdf";
-	const pdfBase64 = await getPdfBase64();
-	await uploadToRepo(octokit, PDF_PATH, pdfBase64, OWNER, REPO, BRANCH);
+  const OWNER = "ashuvssut";
+  const REPO = "ashuvssut-resume";
+  const BRANCH = "download";
+  const PDF_PATH = "Resume _ Ashutosh Khanduala.pdf";
+  const pdfBase64 = await getPdfBase64();
+  await uploadToRepo(octokit, PDF_PATH, pdfBase64, OWNER, REPO, BRANCH);
 };
 
 main();
